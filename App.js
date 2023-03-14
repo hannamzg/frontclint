db85abe4b225d05e46fed1e79c1629b9f7aa73f0
hanna mzeget <elimzghanna123@gmail.com>
2023-03-14 21:16:48 +0200

    safdfew

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import * as React from 'react';
-
+import { Switch } from 'react-native-paper';
 export default function App() {
   const [searchQuery, setSearchQuery] = React.useState('');
 
   const onChangeSearch = query => setSearchQuery(query);
 
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
 
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -16,6 +18,7 @@ export default function App() {
       onChangeText={onChangeSearch}
       value={searchQuery}
     />
+     <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
     </View>
   );
 }
